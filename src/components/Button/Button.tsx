6af2aa146269af0363af14dec47c8c6884e96af9
Jindrich Susen <jindrich.susen@advantages.cz>
2021-11-23 13:57:43 +0100

    + className to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,11 +4,12 @@ import S from "./Button.module.scss";
 export class Button extends React.Component<{
   onClick: () => void,
   label: string,
+  className?: string;
   changeColorOnFocus?: boolean
 }> {
   render() {
     return <button
-      className={S.button + " " + (this.props.changeColorOnFocus ? S.focusedButton : "")}
+      className={S.button + " " + (this.props.className ?? "") + (this.props.changeColorOnFocus ? S.focusedButton : "")}
       onClick={() => this.props.onClick()}>{this.props.label}</button>
   }
 }
